0e570234e220b83bb2d4db74f18a8f55b8e7ee61
fajarnurwahid <fajarnurwahid12@gmail.com>
2021-09-29 22:36:12 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const allMenu = document.querySelectorAll('.side-menu:not(.bottom) li');
+
+allMenu.forEach(item=> {
+	const a = item.querySelector('a');
+	a.addEventListener('click', function () {
+		allMenu.forEach(item1=> item1.classList.remove('active'))
+		this.parentElement.classList.add('active');
+	})
+})
+
+
+
+
+// Toggle sidebar
+const menuBar = document.querySelector('#content nav .menu-bar');
+const sideBar = document.querySelector('#sidebar');
+
+menuBar.addEventListener('click', function () {
+	sideBar.classList.toggle('hide');
+})
