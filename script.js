a4f6427d2463c452962eaa3b1290bb77b6287786
Fajar Nur Wahid <64387801+fajarnurwahid@users.noreply.github.com>
2021-10-02 20:50:22 +0700

    Add files via upload
    Change file

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
-const allMenu = document.querySelectorAll('.side-menu:not(.bottom) li');
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
 
-allMenu.forEach(item=> {
-	const a = item.querySelector('a');
-	a.addEventListener('click', function () {
-		allMenu.forEach(item1=> item1.classList.remove('active'))
-		this.parentElement.classList.add('active');
+allSideMenu.forEach(item=> {
+	const li = item.parentElement;
+
+	item.addEventListener('click', function () {
+		allSideMenu.forEach(i=> {
+			i.parentElement.classList.remove('active');
+		})
+		li.classList.add('active');
 	})
-})
+});
 
 
 
 
-// Toggle sidebar
-const menuBar = document.querySelector('#content nav .menu-bar');
-const sideBar = document.querySelector('#sidebar');
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
 
 menuBar.addEventListener('click', function () {
-	sideBar.classList.toggle('hide');
+	sidebar.classList.toggle('hide');
 })
 
 
 
-if(window.innerWidth < 1200) {
-	sideBar.classList.add('hide');
+
+
+
+
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function (e) {
+	if(window.innerWidth < 576) {
+		e.preventDefault();
+		searchForm.classList.toggle('show');
+		if(searchForm.classList.contains('show')) {
+			searchButtonIcon.classList.replace('bx-search', 'bx-x');
+		} else {
+			searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		}
+	}
+})
+
+
+
+
+
+if(window.innerWidth < 768) {
+	sidebar.classList.add('hide');
+} else if(window.innerWidth > 576) {
+	searchButtonIcon.classList.replace('bx-x', 'bx-search');
+	searchForm.classList.remove('show');
 }
+
+
+window.addEventListener('resize', function () {
+	if(this.innerWidth > 576) {
+		searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		searchForm.classList.remove('show');
+	}
+})
